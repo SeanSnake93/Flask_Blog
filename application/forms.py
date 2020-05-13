41b20467e7ae559bade650a0ac1175adb79cacbf
SeanSnake93 <SeanSnake93@hotmail.co.uk>
2020-05-13 16:15:01 +0000

    Back Up

--- application/forms.py
+++ application/forms.py
@@ -1,34 +1,82 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from application.models import Users
 
 class PostForm(FlaskForm):
     
     first_name = StringField("First Name",
-        validatots=[
+        validators=[
             DataRequired(),
             Length(min=2, max=30)
             ]
     )
 
     last_name = StringField("Last Name",
-        validatots=[
+        validators=[
             DataRequired(),
             Length(min=3, max=30)
             ]
     )
 
     title = StringField("Title",
-        validatots=[
+        validators=[
             DataRequired(),
             Length(min=3, max=30)
             ]
     )
 
-    Content = StringField("Content",
-        validatots=[
+    content = StringField("Content",
+        validators=[
             DataRequired(),
             Length(min=3, max=1000)
             ]
     )
     submit = SubmitField('Post!')
+
+class RegistrationForm(FlaskForm):
+
+    email = StringField("Email",
+        validators=[
+            DataRequired(),
+            Email()
+            ]
+    )
+
+    password = PasswordField("Password",
+        validators=[
+            DataRequired(),
+            ]
+    )
+
+    confirm_password = PasswordField("Comfirm Password",
+        validators=[
+            DataRequired(),
+            EqualTo('password')
+            ]
+    )
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self, email):
+        user = Users.query.filter_by(email=email.data).first()
+
+        if user:
+            raise ValidationError('Email already in use')
+
+class LoginForm(FlaskForm):
+
+    email = StringField("Email",
+        validators=[
+            DataRequired(),
+            Email()
+            ]
+    )
+
+    password = PasswordField("Password",
+        validators=[
+            DataRequired(),
+            ]
+    )
+
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
